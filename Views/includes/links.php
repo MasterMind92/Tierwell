9e89796672632d1ab6023b549c4be60bea74de05
MasterMind92 <yakuzaken92@gmail.com>
2017-11-13 10:45:49 +0000

    Voici le taf

---
+++ Views/includes/links.php
@@ -0,0 +1,12 @@
+<?php  
+#LIEN CSS ENTETE
+define("HEADER", "<link href=\"../ressources/vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">
+    <link href=\"../ressources/css/modern-business.css\" rel=\"stylesheet\">");
+
+#LIEN PIED PAGE
+define("FOOTER", "<script src=\"../ressources/vendor/jquery/jquery.min.js\"></script>
+	<script src=\"../ressources/vendor/popper/popper.min.js\"></script>
+    <script src=\"../ressources/vendor/bootstrap/js/bootstrap.min.js\"></script>")
+
+
+?>
---
+++ Views/sidebar.php
@@ -0,0 +1,86 @@
+<?php include 'includes/links.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Modern Business - Start Bootstrap Template</title>
+
+    <?php echo HEADER; ?>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include'includes/nav.php'; ?>
+   
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3">Sidebar Page
+        <small>Subheading</small>
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.html">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+      </ol>
+
+      <!-- Content Row -->
+      <div class="row">
+        <!-- Sidebar Column -->
+        <div class="col-lg-3 mb-4">
+          <div class="list-group">
+            <a href="index.php" class="list-group-item">Home</a>
+            <a href="about.php" class="list-group-item">About</a>
+            <a href="services.php" class="list-group-item">Services</a>
+            <a href="contact.php" class="list-group-item">Contact</a>
+            <a href="portfolio-1-col.php" class="list-group-item">1 Column Portfolio</a>
+            <a href="portfolio-2-col.php" class="list-group-item">2 Column Portfolio</a>
+            <a href="portfolio-3-col.php" class="list-group-item">3 Column Portfolio</a>
+            <a href="portfolio-4-col.php" class="list-group-item">4 Column Portfolio</a>
+            <a href="portfolio-item.php" class="list-group-item">Single Portfolio Item</a>
+            <a href="blog-home-1.php" class="list-group-item">Blog Home 1</a>
+            <a href="blog-home-2.php" class="list-group-item">Blog Home 2</a>
+            <a href="blog-post.php" class="list-group-item">Blog Post</a>
+            <a href="full-width.php" class="list-group-item">Full Width Page</a>
+            <a href="sidebar.php" class="list-group-item active">Sidebar Page</a>
+            <a href="faq.php" class="list-group-item">FAQ</a>
+            <a href="404.php" class="list-group-item">404</a>
+            <a href="pricing.php" class="list-group-item">Pricing Table</a>
+          </div>
+        </div>
+        <!-- Content Column -->
+        <div class="col-lg-9 mb-4">
+          <h2>Section Heading</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, et temporibus, facere perferendis veniam beatae non debitis, numquam blanditiis necessitatibus vel mollitia dolorum laudantium, voluptate dolores iure maxime ducimus fugit.</p>
+        </div>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <?php echo FOOTER; ?>
+
+  </body>
+
+</html>
